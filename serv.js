bcee304238d517bb5c1e512bdcc30233f1120a82
Juan Pablo <juanpabloalarcon2@gmail.com>
2014-02-23 14:35:41 -0300

    Fix response content Stringify2

--- serv.js
+++ serv.js
@@ -1,16 +1,17 @@
 var http = require('http');
 var pg = require('pg');
-
+var rs;
 http.createServer(function(request,response){
 
 pg.connect(process.env.HEROKU_POSTGRESQL_JADE_URL, function(err, client, done) {
   client.query('SELECT * FROM users', function(err, result) {
     done();
     if(err) return console.error(err);
-    console.log(result.rows);
+	rs=result.rows;    
+	console.log(result.rows);
 
     response.writeHead(200,{"Content-Type": "application/json"});
-    response.end(JSON.stringify(_.flatten(_.compact(result.rows))));
+    response.end(JSON.stringify(_.flatten(_.compact(rs))));
   });
 });
 
